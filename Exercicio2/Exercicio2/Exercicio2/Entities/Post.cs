d6e4b4fffd9c8e19bcc897a729e28ab52ae4c145
jeferson Augusto <carvalho.jeferson@gmail.com>
2020-03-26 20:19:01 -0300

    Criando métodos de Post

--- Exercicio2/Exercicio2/Exercicio2/Entities/Post.cs
+++ Exercicio2/Exercicio2/Exercicio2/Entities/Post.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using Exercicio2.Entities;
 
 namespace Exercicio2.Entities
 {
@@ -10,12 +10,26 @@ namespace Exercicio2.Entities
         public DateTime Moment { get; set; }
         public string Title { get; set; }
         public int Likes { get; set; }
-        public Comment Comments { get; set; }
+        public string Content { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
 
         public Post(DateTime moment, string title)
         {
             Moment = moment;
             Title = title;
         }
+
+
+        public void AddComent(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+
+        public void RemovComment(Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+
     }
 }
