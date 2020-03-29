17b3f7d1f1207e169f6a03b2a01eae9a4c1f56fd
jeferson Augusto <carvalho.jeferson@gmail.com>
2020-03-29 18:38:49 -0300

    criando rotina principal

--- Exercicio4/Empresa/Empresa/Entities/Employee.cs
+++ Exercicio4/Empresa/Empresa/Entities/Employee.cs
@@ -10,6 +10,17 @@ namespace Empresa.Entities
         public int Hours { get; set; }
         public double ValuePerHour { get; set; }
 
+        public Employee()
+        {
+        }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
         public virtual double Payment()
         {
             return ValuePerHour * Hours;
--- Exercicio4/Empresa/Empresa/Program.cs
+++ Exercicio4/Empresa/Empresa/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using Empresa.Entities;
 namespace Empresa
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter with the number of employee: ");
+            int nFunc = int.Parse(Console.ReadLine());
+            List<Employee> empl = new List<Employee>();
+            for (int i = 1; i <= nFunc; i++)
+            {
+                Console.WriteLine("Employee data Nº{0}:", i);
+                Console.Write("Is Outsourced?  (y/n): ");
+                string type = Console.ReadLine();
+                Console.Write("name: ");
+                string nameTemp = Console.ReadLine();
+                Console.WriteLine("Hours: ");
+                int hourTemp = int.Parse(Console.ReadLine());
+
+
+            }
         }
     }
 }
