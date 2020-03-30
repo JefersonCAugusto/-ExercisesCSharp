c7dfd10ea6a757069aea1e37698cf69769460e46
jeferson Augusto <carvalho.jeferson@gmail.com>
2020-03-30 14:31:18 -0300

    apagando namesspace desnecessário

--- Exercício 6/CalcArea/CalcArea/Entities/Enums/Color.cs
+++ Exercício 6/CalcArea/CalcArea/Entities/Enums/Color.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace CalcArea.Entities.Enums
 {
     enum Color : int
--- Exercício 6/CalcArea/CalcArea/Entities/Shape.cs
+++ Exercício 6/CalcArea/CalcArea/Entities/Shape.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 using CalcArea.Entities.Enums;
-using CalcArea.Entities;
 
 namespace CalcArea.Entities
 {
