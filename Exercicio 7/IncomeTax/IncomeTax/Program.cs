195649aa747fbf4a50dc7f7587ef7844d1927d5f
jeferson Augusto <carvalho.jeferson@gmail.com>
2020-03-30 20:52:06 -0300

    Iniciando Exercício 7. IncameTax.

---
+++ Exercicio 7/IncomeTax/IncomeTax/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IncomeTax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
