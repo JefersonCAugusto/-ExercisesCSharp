d6a2024f02b83935de50ffdfe8c2da841a565244
jeferson Augusto <carvalho.jeferson@gmail.com>
2020-03-29 22:59:12 -0300

    classe UsedProduct pronta. criando a rotina principal

--- Exercico 5/Products/Products/Program.cs
+++ Exercico 5/Products/Products/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using Products.Entities;
 namespace Products
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter with de number of products");
+            int n1 = int.Parse(Console.ReadLine());
+            List<Product> p1 = new List<Product>();
+            for (int i = 0; i < n1; i++)
+            {
+                Console.Write("This product is:\nCommon(C), Used(U) or Imported(I)");
+                string type = Console.ReadLine().ToUpper();
+                Console.WriteLine("Product #{0}: ", i + 1);
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine());
+
+                switch (type)
+                {
+                    case "C": Product c1 = new Product(name, price);
+                        p1.Add(c1);
+                        break;
+                    case "U": Console.Write("Manufacture Date: (DD/MM/YYY): ");
+                }
+            }
+            }
         }
     }
-}
