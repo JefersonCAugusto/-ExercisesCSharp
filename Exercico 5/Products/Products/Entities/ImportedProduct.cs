4a35bac935740fc411658ed021ae1ce635afd901
jeferson Augusto <carvalho.jeferson@gmail.com>
2020-03-29 22:34:28 -0300

    classe ImportedProduct pronta

--- Exercico 5/Products/Products/Entities/ImportedProduct.cs
+++ Exercico 5/Products/Products/Entities/ImportedProduct.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace Products.Entities
 {
-    class ImportedProduct
+    class ImportedProduct : Product
     {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct(double customsFee, string name, double price) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+
+        }
+        public override string PriceTag()
+        {
+               
+            return $"Name: {Name} ${TotalPrice()}";
+
+        }
     }
 }
