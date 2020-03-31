b599906dd99fa29793beeb44fd7c4b2b399350f5
jeferson Augusto <carvalho.jeferson@gmail.com>
2020-03-30 23:10:04 -0300

    classe Individual criada. Iniciando Rotina principal

--- Exercicio 7/IncomeTax/IncomeTax/Entities/Individual.cs
+++ Exercicio 7/IncomeTax/IncomeTax/Entities/Individual.cs
@@ -4,7 +4,34 @@ using System.Text;
 
 namespace IncomeTax.Entities
 {
-    class Individual
+    class Individual : TaxPayer
     {
+        public double HealthSpent { get; set; } = 0;
+
+        public Individual(double healthSpent, string name, double anualIncame) : base(name, anualIncame)
+        {
+            HealthSpent = healthSpent;
+        }
+
+        public override double TotalTax()
+        {
+            if (AnualIncame <= 20000)
+            {
+                if (HealthSpent > 0)
+                    return (AnualIncame * 0.15) - (HealthSpent * 0.5);
+                else
+                    return (AnualIncame * 0.15);
+            }
+            else
+            {
+                if (HealthSpent > 0)
+                    return (AnualIncame * 0.25) - (HealthSpent * 0.5);
+                else
+                    return (AnualIncame * 0.25);
+
+            }
+
+        }
+
     }
 }
