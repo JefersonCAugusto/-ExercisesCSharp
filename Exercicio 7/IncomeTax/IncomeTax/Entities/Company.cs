fcba01c687f3955e87b4264712f7dd258fda280b
jeferson Augusto <carvalho.jeferson@gmail.com>
2020-03-30 22:50:57 -0300

    classe Company criada. Vou começar a elaborar a classe Individual

--- Exercicio 7/IncomeTax/IncomeTax/Entities/Company.cs
+++ Exercicio 7/IncomeTax/IncomeTax/Entities/Company.cs
@@ -2,9 +2,24 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace IncomeTax.Entities
 {
-    class Company
+    class Company  : TaxPayer
     {
+        public int NumbersOfEmpoyees { get; set; }
+
+        public Company(int numbersOfEmpoyees, string name, double anualIncame):base(name,anualIncame)
+        {
+            NumbersOfEmpoyees = numbersOfEmpoyees;
+        }
+
+        public override double TotalTax()
+        {
+            if (NumbersOfEmpoyees <= 10)
+                return AnualIncame * 0.16;
+            else
+                return AnualIncame * 0.14;
+        }
     }
 }
